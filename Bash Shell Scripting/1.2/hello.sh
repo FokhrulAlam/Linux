c3243eeac1f9875cdaecf48aaa7ffb0de9610b92
Md Fokhrul Alam <55650168+FokhrulAlam@users.noreply.github.com>
2019-10-15 08:32:38 -0500

    Add files via upload

---
+++ Bash Shell Scripting/1.2/hello.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+#this is the second file
+
+echo "Hello world"   #Saying hello to the world
+
+echo The location of bash is  $BASH	#BASH is a system varible which gives the location of bash
+echo bash version: $BASH_VERSION	#Shows the bash version
+echo $HOME	#Shows the home directory
+echo $PWD	#Shows the current working directory
+
+name1="Md Fokhrul Alam"	#I defined a variable.Variable name can't begin with a number
+echo My name is $name1	#BROTHER OF PRINT FUNCTION OF PYTHON
+graduation=2019
+echo I graduated in $graduation
