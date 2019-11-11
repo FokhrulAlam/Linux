38cbfb36885a0f841b2d55d3c29c7376888c2ae6
Md Fokhrul Alam <55650168+FokhrulAlam@users.noreply.github.com>
2019-11-11 12:59:49 -0600

    Update random.sh

--- Bash Shell Scripting/Projects/Password Generation/Part_1/random.sh
+++ Bash Shell Scripting/Projects/Password Generation/Part_1/random.sh
@@ -17,3 +17,13 @@ echo $password
 #Use nanoseconds as part of the password.
 password=$(date +%s%N)
 echo "$password"
+
+#A better password than the previous ones. We are taking the first 32 characte as the password.
+password=$(date +%s%N | sha256sum | head -c32)
+echo "$password"
+
+
+# An even better password. We are adding some random values too
+password=$(date +%s%N{RANDOM}{RANDOM} | sha256sum | head -c48)
+echo "$password"
+
